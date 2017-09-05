05c871553e83d96c884af8ae8f92db97bd76b0a1
Yuvaraj Yadav <yuvaraj119@gmail.com>
2017-09-05 09:01:37 +0530

    local notification

--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -3,6 +3,8 @@ import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Dish } from './../../shared/dish';
 import { DishProvider } from './../dish/dish';
+import { Storage } from '@ionic/storage';
+import { LocalNotifications } from '@ionic-native/local-notifications'
 import 'rxjs/add/operator/map';
 
 /*
@@ -17,14 +19,34 @@ export class FavoriteProvider {
   favorites: Array<any>;
 
   constructor(public http: Http,
-    private dishservice: DishProvider) {
+    private dishservice: DishProvider,
+    private storage: Storage,
+    private localNotifications: LocalNotifications) {
     console.log('Hello FavoriteProvider Provider');
     this.favorites=[];
+
+    storage.get('favorites').then(favorites => {
+      if(favorites){
+        console.log(favorites);
+        this.favorites = favorites;
+      }else{
+        console.log('favorites not defined');
+      }
+    });
+
   }
 
   addFavorite(id: number){
-    if(!this.isFavorite(id))
+    if(!this.isFavorite(id)){
       this.favorites.push(id);
+      this.storage.set('favorites',this.favorites);
+
+      this.localNotifications.schedule({
+        id: 1,
+        text: 'Dish '+id+' added as a favorite successfully'
+      });
+    }
+    console.log('favorites',this.favorites);
     return true;
   }
 
@@ -33,6 +55,12 @@ export class FavoriteProvider {
   }
 
   getFavorites(): Observable<Dish[]>{
+    this.localNotifications.schedule({
+      id: 1,
+      text: 'Dish added as a favorite successfully',
+      icon: 'ic_notifications',
+      smallIcon: 'ic_notifications_small'
+    });
     return this.dishservice.getDishes()
       .map(dishes => dishes.filter(dish => this.favorites.some(el => el === dish.id)));
   }
