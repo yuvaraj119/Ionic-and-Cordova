46209132214454cf55473afee0b40e1911acf269
Yuvaraj Yadav <yuvaraj119@gmail.com>
2017-09-06 08:45:28 +0530

    camera and registration completed

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, ModalController } from 'ionic-angular';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Storage } from '@ionic/storage'
 import { User } from './../../shared/user';
+import { RegisterPage } from './../register/register';
 
 /**
  * Generated class for the LoginPage page.
@@ -25,6 +26,7 @@ export class LoginPage {
     public navParams: NavParams,
     private viewCtrl: ViewController,
     private formBuilder: FormBuilder,
+    private modalCtrl: ModalController,
     private storage: Storage) {
 
       this.loginForm = this.formBuilder.group({
@@ -67,6 +69,12 @@ export class LoginPage {
     }
     this.viewCtrl.dismiss();
 
+  }
+
+  openRegister(){
+    let modal=this.modalCtrl.create(RegisterPage);
+    modal.present();
+    modal.onDidDismiss(() => this.dismissLogin());
   }
 
 }
